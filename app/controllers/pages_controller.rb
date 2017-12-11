165249252d208b14486bf1d6112430cde4042327
Armand Fardeau <fardeauarmand@gmail.com>
2017-12-11 23:38:56 +0100

    refactor controller pages action search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,18 +4,11 @@ class PagesController < ApplicationController
   end
 
   def search
-    if params[:search].present? && params[:search].strip != ''
-      session[:aiirbnb_search] = params[:search]
-    end
-    if session[:aiirbnb_search] && session[:aiirbnb_search] != ''
-      @rooms_address = Room.where(active: true).near(session[:aiirbnb_search], 5, order: 'distance')
-    else
-      @rooms_address = Room.where(active: true).all
-    end
+    search_places(params[:search])
     @search = @rooms_address.ransack(params[:q])
     @rooms = @search.result
-    @arrRooms = @rooms.to_a
-    if (params[:start_date] && params[:end_date] && !params[:start_date].empty? & !params[:end_date].empty?)
+    @arr_rooms = @rooms.to_a
+    if params[:start_date] && params[:end_date] && !params[:start_date].empty? & !params[:end_date].empty?
       start_date = Date.parse(params[:start_date])
       end_date = Date.parse(params[:end_date])
       @rooms.each do |room|
@@ -23,9 +16,18 @@ class PagesController < ApplicationController
             (? <= end_date AND end_date <= ?) OR (start_date < ? AND ? < end_date)', start_date, end_date,
                                                 start_date, end_date, start_date, end_date).limit(1)
         if not_available.length > 0
-          @arrRooms.delete(room)
+          @arr_rooms.delete(room)
         end
       end
+    end
+  end
+
+  def search_places(param)
+    session[:aiirbnb_search] = param if param.present? && param.strip != ''
+    if session[:aiirbnb_search] && session[:aiirbnb_search] != ''
+      @rooms_address = Room.where(active: true).near(session[:aiirbnb_search], 5, order: 'distance')
+    else
+      @rooms_address = Room.where(active: true).all
     end
   end
 end
