0cf8e6196348777dc3d913a0e0474a0fedd3090e
Armand Fardeau <fardeauarmand@gmail.com>
2017-12-11 14:07:08 +0100

    add some helpers to keep code dry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
       'default_profil_picture.svg'
     end
   end
+
+  def first_room_photos(room, size = :medium)
+    return 'default_room_photos.svg' if room.photos.blank? || room.nil?
+    if room.photos.length > 0
+      room.photos[0].image.url(size)
+    end
+  end
 end
--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,9 @@
 module RoomsHelper
+  def nice_display_of_carousel(room, size = :original)
+    render partial: 'rooms/partials/carousel', locals: {room: room, size: size}
+  end
+
+  def nice_display_of_google_maps(room, height)
+    render partial: 'rooms/partials/google_maps', locals: {room: room, height: height}
+  end
 end
