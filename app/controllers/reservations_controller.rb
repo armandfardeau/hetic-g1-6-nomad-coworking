9847000323eb962618a9ba55d5aa49225f129160
Armand Fardeau <fardeauarmand@gmail.com>
2017-12-11 09:28:27 +0100

    add scopes to keep controller dry

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,7 +6,32 @@ class ReservationsController < ApplicationController
     redirect_to @reservation.room, notice: 'Your booking has been accepted.'
   end
 
+  def preload
+    room = Room.find(params[:room_id])
+    reservations = room.reservations.bookable
+    render json: reservations
+  end
+
+  def preview
+    start_date = Date.parse(params[:start_date])
+    end_date = Date.parse(params[:end_date])
+    output = {
+        conflict: conflict(start_date, end_date)
+    }
+    render json: output
+  end
+
+  def trips
+    @trips = current_user.reservations
+  end
+
   private
+
+  def conflict(start_date, end_date)
+    room = Room.find(params[:room_id])
+    check = room.reservations.available(start_date, end_date)
+    check unless check.empty?
+  end
 
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
