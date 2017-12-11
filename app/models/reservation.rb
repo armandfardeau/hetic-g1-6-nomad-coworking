c685de7773736a3616d282412f6f49dc0095ab76
Armand Fardeau <fardeauarmand@gmail.com>
2017-12-11 09:29:42 +0100

    fix rubocop offenses

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,6 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
-  scope :available, ->(start_date, end_date) { where("? < start_date AND end_date < ?", start_date, end_date)}
-  scope :bookable, ->(today = Date.today) { where("start_date >= ? OR end_date >= ?", today, today)}
+  scope :available, ->(start_date, end_date) {where('? < start_date AND end_date < ?', start_date, end_date)}
+  scope :bookable, ->(today = Time.zone.today) {where('start_date >= ? OR end_date >= ?', today, today)}
 end
