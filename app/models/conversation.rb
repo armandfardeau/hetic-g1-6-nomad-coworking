b19e4bcc9604780480c0600f8ce2bf5730ea022e
Armand Fardeau <fardeauarmand@gmail.com>
2017-12-14 21:12:59 +0100

    fix messages trouble ! Damn rails UJS

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,7 +2,7 @@ class Conversation < ApplicationRecord
   belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
   has_many :messages, dependent: :destroy
-  validates :sender_id, scope: :recipient_id, uniqueness: true
+  validates :sender_id, uniqueness: {scope: :recipient_id}
 
   scope :involving, ->(user) {where('conversations.sender_id = ? OR conversations.recipient_id = ?', user.id, user.id)}
 
