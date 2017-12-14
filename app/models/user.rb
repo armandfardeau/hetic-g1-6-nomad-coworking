6d2ac5c610d69f8fd98bde4e6bd937065035e637
Armand Fardeau <fardeauarmand@gmail.com>
2017-12-14 09:58:58 +0100

    improve chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
 
   has_attached_file :avatar,
                     styles: {medium: '300x300>', thumb: '100x100>'},
-                    default_url: '/assets/default_profil_picture.svg'
+                    default_url: '/assets/images/default_profil_picture.svg'
   validates_attachment_content_type :avatar, content_type: %w[image/jpg image/jpeg image/png]
 end
