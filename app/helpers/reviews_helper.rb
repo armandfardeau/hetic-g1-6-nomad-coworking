c83e7250b00abba6dd2c34384b86455ec48bc2cb
Armand Fardeau <fardeauarmand@gmail.com>
2017-12-15 14:46:33 +0100

    fix review form

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,6 +1,6 @@
 module ReviewsHelper
-  def nice_display_delete_comment(room, review, user = current_user)
+  def nice_display_delete_comment(office, review, user = current_user)
     return unless user && user == review.user
-    render partial: 'reviews/partials/destroy_link', locals: {room: room, r: review}
+    render partial: 'reviews/partials/destroy_link', locals: {office: office, r: review}
   end
 end
